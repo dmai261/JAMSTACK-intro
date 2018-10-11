5f3ac8041ecf9c847af080903b6d4b89e8d7b5c2
David <david@davids-air.lan>
2018-10-11 14:33:42 -0700

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hello world!</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Greetings, my name is David Mai and I am a Full stack software developer.</p>
+    <p>My name is David Mai and I am a Full stack software developer.</p>
     <p>Looking forward to this Hackathon!!!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
